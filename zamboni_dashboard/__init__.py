a7577bde499452cc6878a39e8a023f69945c5d7c
Jeremiah Orem <jeremy.orem@gmail.com>
2012-06-21 15:24:28 -0700

    Add sentry support.

--- zamboni_dashboard/__init__.py
+++ zamboni_dashboard/__init__.py
@@ -6,4 +6,8 @@ from .settings_local import LocalSettings
 app = Flask(__name__)
 app.config.from_object(LocalSettings)
 
+if app.config.get('SENTRY_DSN'):
+    from raven.contrib.flask import Sentry
+    sentry = Sentry(app)
+
 from . import views
